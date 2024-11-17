023492a4a2a0ecf61d37e81bc8b68532cef370e1
developerVictoria <zubannekoviktoriia@gmail.com>
2024-11-17 19:53:27 +0200

    85

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,25 @@
-import express from 'express';
-import {PORT} from "./config"
+import express, { Request, Response } from 'express';
+import {PORT, mongoDBURL} from "./config"
+import mongoose from 'mongoose'
+
 
 const app = express();
 
+app.get('/', (req :Request, res: Response) =>{
+    console.log(req);
+    res.status(234).send('Basic MERN bookshop');
+    return;
+});
 
-app.listen(PORT, ()=>{
-    console.log(`App runing at port: ${PORT}`)
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to the db')
+     app.listen(PORT, ()=>{
+        console.log(`App runing at port: ${PORT}`)
+    })
+})
+.catch((err)=>{
+console.error(err);
 })
