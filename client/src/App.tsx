a2a1022ac43aa5fe8564ccadf96e26d1a90c1bd7
developerVictoria <zubannekoviktoriia@gmail.com>
2024-11-20 20:19:25 +0200

    update pages and router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
-      <Route path='' element={}/>
+      <Route path='/' element={<Home />}/>
+      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/delete' element={<DeleteBook />}/>
+      <Route path='/books' element={<ShowBook />}/>
+      <Route path='/books/edit' element={<EditBook />}/>
     </Routes>
 
     
