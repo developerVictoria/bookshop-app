252e0a22172856c1f4de96f35fd0c3a3af220b6e
developerVictoria <zubannekoviktoriia@gmail.com>
2024-11-21 23:16:42 +0200

    update the Create book with the logic for the request to create the book

--- client/src/pages/CreateBook.tsx
+++ client/src/pages/CreateBook.tsx
@@ -1,6 +1,27 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import {getAllBooksRoute} from './config.tsx'
+import { BookType } from '../types/BookTypes.tsx';
 
 
 const CreateBook = () => {
+  const[book, setBook] = useState<BookType>({author:'', title:'', publishYear:0});
+  const[loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handeSaveBooks = ()=>{
+    setLoading(true);
+    axios
+        .post(getAllBooksRoute, book)
+        .then(()=>{
+          setLoading(false);
+          navigate('/');
+        })
+  }
+
   return (
     <div>
       
